6921758a98f0ecf666092c5d55f6d2baf49a114d
Chris Hill-Scott <me@quis.cc>
2019-10-14 14:34:08 +0100

    initial stab

--- app/routes.js
+++ app/routes.js
@@ -3,4 +3,26 @@ const router = express.Router()
 
 // Add your routes here - above the module.exports line
 
+router.post('/where-do-you-live', function (req, res) {
+
+  let whereDoYouLive = req.session.data['where-do-you-live']
+
+  if (whereDoYouLive === 'another-country') {
+    res.redirect('/how-do-you-want-to-register')
+  }
+
+  res.redirect('/uk-journey')
+
+})
+
+
+router.post('/how-do-you-want-to-register', function (req, res) {
+
+  let howDoYouWantToRegister = req.session.data['how-do-you-want-to-register']
+
+  res.redirect('/overseas-' + howDoYouWantToRegister)
+
+})
+
+
 module.exports = router
